456b7a1fbec95196427aff0b02ae94630aed21c4
David Fornander <83448440+DavidFornander@users.noreply.github.com>
2025-03-26 11:11:01 +0100

    feat: wrap application in ThemeProvider for improved theming support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '../contexts/ThemeContext';
 import Layout from '../components/Layout';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
-};
+}
 
 export default MyApp;
